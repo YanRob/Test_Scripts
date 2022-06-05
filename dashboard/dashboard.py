4357a58b14efad774b694bc88824d6ce9f6d3bca
YanRob <robyan@netcourrier.com>
2022-06-05 17:06:40 +0200

    Add legend and color to plot

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -8,12 +8,13 @@ from bokeh.layouts import row, column
 class DashBoard:
     def __init__(self, pathname):
         self.data = pd.read_excel(pathname, engine='openpyxl')
+        self.options = list(self.data['C'].unique())
+        self.selected_data = self.data[self.data['C'] == self.options[0]]
+
         self.source = None
         self.layout = None
         self.plot = None
         self.layout = None
-
-        self.options = list(self.data['C'].unique())
         self.select = None
 
     def create_layout(self):
@@ -23,20 +24,20 @@ class DashBoard:
 
     def make_plot(self):
         p = figure(plot_height=300, plot_width=1200, toolbar_location='above')
-        p.line('x', 'A', source=self.source)
-        p.circle('x', 'B', source=self.source)
+        p.line('x', 'A', source=self.source, legend='Ay', color='red')
+        p.circle('x', 'B', source=self.source, legend='By', color='green')
         self.plot = p
 
     def make_select(self):
 
         def update(attr, old, new):
             selected_option = self.select.value
-            df1 = self.data[self.data['C'] == selected_option]
-            newSource = ColumnDataSource(df1)
+            self.selected_data = self.data[self.data['C'] == selected_option]
+            newSource = ColumnDataSource(self.selected_data)
             self.source.data = newSource.data
 
         self.select = Select(title="C", value=self.options[0], options=self.options)
         self.select.on_change('value', update)
 
     def source_data(self):
-        self.source = ColumnDataSource(data=self.data)
+        self.source = ColumnDataSource(data=self.selected_data)
